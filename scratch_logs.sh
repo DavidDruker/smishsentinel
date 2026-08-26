4a5b06252445942df70d6ac09be69c50ef10f5a7
David Druker <dave.druker@gmail.com>
2026-08-26 12:41:49 -0400

    Remove accidentally-committed scratch debug files, ignore them going forward

--- scratch_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export AWS_SHARED_CREDENTIALS_FILE=/mnt/c/Users/DavidNew/.aws/credentials
-export AWS_CONFIG_FILE=/mnt/c/Users/DavidNew/.aws/config
-export AWS_PROFILE=agentsforhumans
-cd /mnt/c/Users/DavidNew/Documents/smishsentinel
-aws logs tail /aws/bedrock-agentcore/runtimes/app-9IoGY77lPi-DEFAULT \
-  --log-stream-name-prefix "2026/08/26/[runtime-logs]" --since 10m --format short \
-  > scratch_logwindow.txt 2>&1
-wc -l scratch_logwindow.txt
